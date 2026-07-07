d94606d7e01d455da0e1ccaa286c65a09465aa5d
KolAB-JonasRing <jonas.ring@caramelofy.se>
2026-07-07 19:13:29 +0200

    feat(web)!: transform console app to handle web inside

--- MXP-Sugar-RealTime_Communication/Program.cs
+++ MXP-Sugar-RealTime_Communication/Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hej från en transformerad console app!");
+
+app.Run();
